3d1b4bc14fda36f9694852eec09009731b67a0b7
pedrohenrique <ph.oliveirasousa@protonmail.com>
2020-05-04 22:15:52 -0300

    Updated Comments and fixed some window bugs

--- src/com/thatmofocompany/entities/Example_1_Game_Window.java
+++ src/com/thatmofocompany/entities/Example_1_Game_Window.java
@@ -6,15 +6,22 @@ import java.awt.Dimension;
 import javax.swing.JFrame;
 
 public class Example_1_Game_Window extends Canvas {
+	
+	// Serialization
+	private static final long serialVersionUID = 1L;
+	
+	// Variables for create window
 	public static JFrame frame;
 	private final int WIDTH = 240;
 	private final int HEIGHT = 160;
 	private final int SCALE = 3;
 	
+	// Constructor
 	public Example_1_Game_Window() {
 		initFrame();
 	}
 	
+	// Main
 	public static void main(String[] args) {
 		Example_1_Game_Window game = new Example_1_Game_Window();
 	}
@@ -23,12 +30,13 @@ public class Example_1_Game_Window extends Canvas {
 		// Create frame object for window
 		// Configure Window Dimensions
 		// Configure close option
-		frame = new JFrame("PH' game");
+		frame = new JFrame("PH's game");
 		frame.setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		// Put the frame on window
-		// Code Comented below for testing purpose 
+		// Adds the "frame" Container into the window itself
+		// Remove the resize option
 		frame.add(this);
 		frame.setResizable(false);
 		
