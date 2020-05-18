119d396b3b8b3fa9e6a0091fb2a2eaef7aa134c6
pedrohenrique <ph.oliveirasousa@protonmail.com>
2020-05-17 22:41:32 -0300

    Example 3 updates - Midnight! What do you want for me?
    Signed-off-by: pedrohenrique <ph.oliveirasousa@protonmail.com>

--- src/com/thatmofocompany/entities/Player.java
+++ src/com/thatmofocompany/entities/Player.java
@@ -8,13 +8,13 @@ import java.awt.image.BufferedImage;
 
 public class Player {
 	// Variables for Render
-	public Spritesheet sheet;
-	public BufferedImage[] player;
+	private Spritesheet sheet;
+	private BufferedImage[] player;
 	
 	// Variables for Tick
 	public boolean right, left, up, down;
-	public int x, y;
-	public int curAnimation = 0;
+	private int x, y;
+	private int curAnimation = 0;
 	
 	// Constructor
 	public Player(int x, int y) {
@@ -33,12 +33,68 @@ public class Player {
 		}
 		else if(left) {
 			x--;
-			if (curAnimation <= 3) {
+			if (curAnimation <= 4) {
 				curAnimation = 4;
 			}
 			curAnimation++;
-			if (curAnimation >= 8) {
+			if (curAnimation > 7) {
 				curAnimation = 4;
+			}
+		}
+		
+		// Up and Down Walking
+		if(up) {
+			y--;
+			if (right) {
+				if (curAnimation >= 3) {
+					curAnimation = 0;
+				}
+				curAnimation++;
+			}
+			else if (left) {
+				if (curAnimation <= 4) {
+					curAnimation = 4;
+				}
+				curAnimation++;
+				if (curAnimation > 7) {
+					curAnimation = 4;
+				}
+			}
+			else {
+				if (curAnimation <= 8) {
+					curAnimation = 8;
+				}
+				curAnimation++;
+				if (curAnimation > 11) {
+					curAnimation = 8;
+				}
+			}
+		}
+		else if(down) {
+			y++;
+			if (right) {
+				if (curAnimation >= 3) {
+					curAnimation = 0;
+				}
+				curAnimation++;
+			}
+			else if (left) {
+				if (curAnimation <= 4) {
+					curAnimation = 4;
+				}
+				curAnimation++;
+				if (curAnimation > 7) {
+					curAnimation = 4;
+				}
+			}
+			else {
+				if (curAnimation <= 12) {
+					curAnimation = 12;
+				}
+				curAnimation++;
+				if (curAnimation > 15) {
+					curAnimation = 12;
+				}
 			}
 		}
 		
@@ -49,15 +105,7 @@ public class Player {
 		else if (x < 0) {
 			x = 0;
 		}
-		
-		// Up and Down Walking
-		if(up) {
-			y--;
-		}
-		else if(down) {
-			y++;
-		}
-		
+
 		// Up and Down Borders
 		if(y > Example_3_Infinity_Loop_Tick.HEIGHT - 16) {
 			y = Example_3_Infinity_Loop_Tick.HEIGHT - 16;
@@ -69,21 +117,33 @@ public class Player {
 	
 	public void render(Graphics g) {
 		sheet = new Spritesheet("/spritesheet.png");
-		player = new BufferedImage[9];
+		player = new BufferedImage[16];
 		
 		// Diferent types of animation on Spritesheet
 		// Certainly could be better
+		// Right
 		player[0] = sheet.getSprite(0, 0, 16, 16);
 		player[1] = sheet.getSprite(16, 0, 16, 16);		
 		player[2] = sheet.getSprite(32, 0, 16, 16);		
 		player[3] = sheet.getSprite(48, 0, 16, 16);
+		// Left
 		player[4] = sheet.getSprite(64, 0, 16, 16);
 		player[5] = sheet.getSprite(80, 0, 16, 16);
 		player[6] = sheet.getSprite(96, 0, 16, 16);
 		player[7] = sheet.getSprite(112, 0, 16, 16);
-		player[8] = sheet.getSprite(128, 0, 16, 16);
+		// Down
+		player[8] = sheet.getSprite(0, 16, 16, 16);
+		player[9] = sheet.getSprite(16, 16, 16, 16);
+		player[10] = sheet.getSprite(32, 16, 16, 16);
+		player[11] = sheet.getSprite(48, 16, 16, 16);
+		// Up
+		player[12] = sheet.getSprite(64, 16, 16, 16);
+		player[13] = sheet.getSprite(80, 16, 16, 16);
+		player[14] = sheet.getSprite(96, 16, 16, 16);
+		player[15] = sheet.getSprite(112, 16, 16, 16);
 		
 		Graphics2D g2 = (Graphics2D) g;
 		g2.drawImage(player[curAnimation], x, y, null);
+		
 	}
 }
